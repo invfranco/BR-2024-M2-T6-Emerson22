e784d3939a1a522af139c225e4a5491c72101b16
Emerson ramos <122629752+invfranco@users.noreply.github.com>
2023-09-13 19:53:31 -0300

    AULA2-Duck-Emerson
    prof tentei implementar com meus amigos e não conseguimos concluir, espero que a senhora considere a nossa explicação.

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,11 +1,10 @@
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-
 from dino_runner.components.dinosaur import Dinosaur
 
 class Game:
-    def _init_(self):
+    def __init__(self):
         pygame.init()
         pygame.display.set_caption(TITLE)
         pygame.display.set_icon(ICON)
@@ -15,17 +14,15 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.player = Dinosaur()
 
     def run(self):
-        # Game loop: events - update - draw
         self.playing = True
         while self.playing:
             self.events()
             self.update()
             self.draw()
-            self.player = Dinosaur()
         pygame.quit()
-
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -34,10 +31,10 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
-         
+
     def draw(self):
         self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255))
+        self.screen.fill((255, 255, 255)) 
         self.draw_background()
         self.player.draw(self.screen)
         pygame.display.update()
